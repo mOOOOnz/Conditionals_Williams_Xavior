641e76f803b2b1abf1596e9ea8dcd07c88eb1b35
mOOOOnz <blackfire0705@gmail.com>
2024-03-13 09:06:13 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Conditionals_Williams_Xavior
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Conditionals_Williams_Xavior
+ 
